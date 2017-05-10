b00647bc080215edf5dc08372f8fb8037f58713a
Umqra <golovoiapklavu@gmail.com>
2017-05-10 21:00:58 +0500

    Добавил проект DragonFractal и переименовал проект
    SimdLecture -> SimdExamples

---
+++ SimdLecture/DragonFractal/Pixels.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Fractals
+{
+	public class Pixels
+	{
+		public readonly List<Tuple<double, double>> PixelsSet;
+
+		public Pixels()
+		{
+			PixelsSet = new List<Tuple<double, double>>();
+		}
+
+		public void SetPixel(double x, double y)
+		{
+			PixelsSet.Add(Tuple.Create(x, y));
+		}
+
+		public void DrawToBitmap(Bitmap image)
+		{
+			if (PixelsSet.Count == 0) return;
+			var maxX = PixelsSet.Max(p => p.Item1);
+			var maxY = PixelsSet.Max(p => p.Item2);
+			var minX = PixelsSet.Min(p => p.Item1);
+			var minY = PixelsSet.Min(p => p.Item2);
+			var width = maxX - minX;
+			var height = maxY - minY;
+		    var scaleX = (image.Width-20) / width;
+			var scaleY = (image.Height-20) / height;
+			var scale = Math.Min(scaleX, scaleY);
+
+            var intensity = new int[image.Width, image.Height];
+		    int maxIntensity = 0;
+            foreach (var pixel in PixelsSet)
+			{
+				var x = (pixel.Item1 - minX - width/2)*scale + image.Width/2.0;
+				var y = (pixel.Item2 - minY - height/2)*scale + image.Height/2.0;
+			    intensity[(int)x, (int)y]++;
+			    maxIntensity = Math.Max(intensity[(int)x, (int)y], maxIntensity);
+			}
+            for (int i = 0; i < intensity.GetLength(0); i++)
+		    for (int s = 0; s < intensity.GetLength(1); s++)
+		    {
+		        var componentValue = 255 * intensity[i, s] / maxIntensity;
+		        image.SetPixel(i, s, Color.FromArgb(componentValue, componentValue, 0));
+		    }
+		}
+	}
+}
---
+++ SimdLecture/DragonFractal/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+// Это пространство имен содержит средства создания оконных приложений. В частности в нем находится класс Form.
+// Для того, чтобы оно стало доступно, в проект был подключен на System.Windows.Forms.dll
+using System.Windows.Forms;
+
+namespace Fractals
+{
+	internal static class Program
+	{
+		private static void Main()
+		{
+			var pixels = new Pixels();
+			var image = new Bitmap(800, 800, PixelFormat.Format24bppRgb);
+			using (var g = Graphics.FromImage(image))
+				g.Clear(Color.Black);
+			DragonFractalTask.DrawDragonFractal(pixels, 10000000, 0);
+			pixels.DrawToBitmap(image);
+
+			// При желании можно сохранить созданное изображение в файл вот так:
+			// image.Save("dragon.png", ImageFormat.Png);
+
+			ShowImageInWindow(image);
+		}
+
+		private static void ShowImageInWindow(Bitmap image)
+		{
+			// Создание нового окна заданного размера:
+			var form = new Form
+			{
+				Text = "Harter–Heighway dragon",
+				ClientSize = image.Size
+			};
+
+			//Добавляем специальный элемент управления PictureBox, который умеет отображать созданное нами изображение.
+			form.Controls.Add(new PictureBox {Image = image, Dock = DockStyle.Fill, SizeMode = PictureBoxSizeMode.CenterImage});
+			form.ShowDialog();
+		}
+	}
+}

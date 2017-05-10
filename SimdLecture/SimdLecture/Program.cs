084d71e5f8d29a465af92ad8c1f2893d40098d08
Umqra <golovoiapklavu@gmail.com>
2017-05-10 13:07:44 +0500

    Исправил первый пример и добавил второй

--- SimdLecture/SimdLecture/Program.cs
+++ SimdLecture/SimdLecture/Program.cs
@@ -1,168 +1,48 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Diagnostics.Eventing.Reader;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Runtime.InteropServices.WindowsRuntime;
-using BenchmarkDotNet.Attributes.Jobs;
 using BenchmarkDotNet.Running;
-using BenchmarkDotNet.Attributes;
 
 namespace SimdLecture
 {
-    [SimpleJob(targetCount: 10, id: "FastBenchmark")]
-    public class SumLoopBenchmark
+    internal class Program
     {
-        public delegate long SumDelegate();
-        private long[] data;
-        private int length;
-
-        public long[] GenerateSequence(int n)
+        private static void EstimateTime<T>(Func<T> sumFunc)
         {
-            var seq = new long[n];
-            for (int i = 0; i < n; i++)
-                seq[i] = i;
-            return seq;
+            Console.WriteLine($"Test method: {sumFunc.Method.Name}");
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var result = sumFunc();
+            stopwatch.Stop();
+            Console.WriteLine($"Result: {result}, Elapsed milliseconds: {stopwatch.ElapsedMilliseconds}");
         }
 
-        public SumLoopBenchmark()
+        private static void TestAllSumImplementations()
         {
-            length = (int)3e7;
-            data = GenerateSequence(length);
+            var instance = new SumLoopBenchmark();
+            EstimateTime(instance.SimdUnrolledForSum);
+            EstimateTime(instance.SimdForSum);
+            EstimateTime(instance.ForeachSum);
+            EstimateTime(instance.UnrolledForSum);
+            EstimateTime(instance.SimpleForSum);
+            EstimateTime(instance.GeneratedUnrolledForSum);
+            EstimateTime(instance.LinqAggregate);
+            EstimateTime(instance.LinqSum);
         }
 
-        [Benchmark]
-        public long LinqSum() => data.Sum(x => x);
-
-        [Benchmark]
-        public long LinqAggregate() => data.Aggregate((long)0, (current, element) => current + element);
-
-        [Benchmark]
-        public long ForeachSum()
+        private static void TestAllSegmentCheckImplementations()
         {
-            long sum = 0;
-            foreach (var element in data)
-                sum += element;
-            return sum;
+            var instance = new SegmentCheckBenchmark();
+            EstimateTime(instance.SimdForCountInRange);
+            EstimateTime(instance.ForeachCountInRange);
+            EstimateTime(instance.SimpleForCountInRange);
         }
 
-        [Benchmark]
-        public long SimpleForSum()
+        private static void Main(string[] args)
         {
-            long sum = 0;
-            for (int i = 0; i < data.Length; i++)
-                sum += data[i];
-            return sum;
-        }
-
-        [Benchmark]
-        public long UnrolledForSum()
-        {
-            long sum = 0;
-            int alignedLength = data.Length - data.Length % 4;
-            for (int i = 0; i < alignedLength; i += 4)
-            {
-                sum += data[i] + data[i + 1] + data[i + 2] + data[i + 3];
-            }
-            for (int i = alignedLength; i < data.Length; i++)
-                sum += data[i];
-            return sum;
-        }
-
-        private Func<int, long> GeneratedUnrolledAlignedSumMethod()
-        {
-            var dynamicMethod = new DynamicMethod(
-                nameof(GeneratedUnrolledAlignedSumMethod), typeof(long), new[] {typeof(SumLoopBenchmark), typeof(int)}, typeof(SumLoopBenchmark));
-
-            GenerateMapMethodBody(dynamicMethod.GetILGenerator());
-
-            return (Func<int, long>)dynamicMethod.CreateDelegate(typeof(Func<int, long>), this);
-        }
-
-        [Benchmark]
-        public long GeneratedUnrolledForSum()
-        {
-            int alignedLength = length - length % 4;
-            var sum = GeneratedUnrolledAlignedSumMethod()(alignedLength);
-            for (int i = alignedLength; i < length; i++)
-                sum += data[i];
-            return sum;
-        }
-
-        private void GenerateMapMethodBody(ILGenerator generator)
-        {
-            var sumVar = generator.DeclareLocal(typeof(long));
-            var dataVar = generator.DeclareLocal(typeof(int[]));
-            var indexVar = generator.DeclareLocal(typeof(int));
-            generator.Emit(OpCodes.Ldc_I4_0);
-            generator.Emit(OpCodes.Conv_I8);
-            generator.Emit(OpCodes.Stloc, sumVar);
-
-            generator.Emit(OpCodes.Ldarg_0);
-            generator.Emit(OpCodes.Ldfld, typeof(SumLoopBenchmark)
-                .GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
-                .Single(f => f.Name == "data"));
-            generator.Emit(OpCodes.Stloc, dataVar);
-
-            var startLoopLabel = generator.DefineLabel();
-            var bodyLoopLabel = generator.DefineLabel();
-            generator.Emit(OpCodes.Ldc_I4_0);
-            generator.Emit(OpCodes.Stloc, indexVar);
-            generator.Emit(OpCodes.Br_S, startLoopLabel);
-
-            generator.MarkLabel(bodyLoopLabel);
-            generator.Emit(OpCodes.Ldloc, sumVar);
-            generator.Emit(OpCodes.Ldloc, dataVar);
-            generator.Emit(OpCodes.Ldloc, indexVar);
-            generator.Emit(OpCodes.Ldelem_I4);
-
-            generator.Emit(OpCodes.Ldloc, dataVar);
-            generator.Emit(OpCodes.Ldloc, indexVar);
-            generator.Emit(OpCodes.Ldc_I4_1);
-            generator.Emit(OpCodes.Add);
-            generator.Emit(OpCodes.Ldelem_I4);
-            generator.Emit(OpCodes.Add);
-
-            generator.Emit(OpCodes.Ldloc, dataVar);
-            generator.Emit(OpCodes.Ldloc, indexVar);
-            generator.Emit(OpCodes.Ldc_I4_2);
-            generator.Emit(OpCodes.Add);
-            generator.Emit(OpCodes.Ldelem_I4);
-            generator.Emit(OpCodes.Add);
-
-            generator.Emit(OpCodes.Ldloc, dataVar);
-            generator.Emit(OpCodes.Ldloc, indexVar);
-            generator.Emit(OpCodes.Ldc_I4_3);
-            generator.Emit(OpCodes.Add);
-            generator.Emit(OpCodes.Ldelem_I4);
-            generator.Emit(OpCodes.Add);
-
-            generator.Emit(OpCodes.Add);
-            generator.Emit(OpCodes.Stloc, sumVar);
-
-            generator.Emit(OpCodes.Ldloc, indexVar);
-            generator.Emit(OpCodes.Ldc_I4_4);
-            generator.Emit(OpCodes.Add);
-            generator.Emit(OpCodes.Stloc, indexVar);
-
-            generator.MarkLabel(startLoopLabel);
-            generator.Emit(OpCodes.Ldloc, indexVar);
-            generator.Emit(OpCodes.Ldarg_1);
-            generator.Emit(OpCodes.Conv_I4);
-
-            generator.Emit(OpCodes.Blt_S, bodyLoopLabel);
-
-            generator.Emit(OpCodes.Ldloc, sumVar);
-            generator.Emit(OpCodes.Ret);
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            BenchmarkRunner.Run<SumLoopBenchmark>();
+            //TestAllSumImplementations();
+            TestAllSegmentCheckImplementations();
+            //BenchmarkRunner.Run<SumLoopBenchmark>();
+            //BenchmarkRunner.Run<SegmentCheckBenchmark>();
         }
     }
 }
---
+++ SimdLecture/SimdLecture/SegmentCheckBenchmark.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+
+namespace SimdLecture
+{
+    [SimpleJob(targetCount: 10, id: "FastBenchmark")]
+    public class SegmentCheckBenchmark
+    {
+        private readonly int[] data;
+
+        public int L { get; set; } = -(int)1e8;
+        
+        public int R { get; set; } = (int)1e9;
+
+        public SegmentCheckBenchmark()
+        {
+            var length = (int)1e8;
+            data = GenerateSequence(length);
+        }
+
+        private int[] GenerateSequence(int n)
+        {
+            var random = new Random(0);
+            var seq = new int[n];
+            for (int i = 0; i < n; i++)
+                seq[i] = random.Next();
+            return seq;
+        }
+
+        [Benchmark]
+        public int ForeachCountInRange()
+        {
+            var count = 0;
+            foreach (var element in data)
+                if (L <= element && element <= R)
+                    count++;
+            return count;
+        }
+
+        [Benchmark]
+        public int SimpleForCountInRange()
+        {
+            var count = 0;
+            for (int i = 0; i < data.Length; i++)
+                if (L <= data[i] && data[i] <= R)
+                    count++;
+            return count;
+        }
+
+        [Benchmark]
+        public int SimdForCountInRange()
+        {
+            var vectorSize = Vector<int>.Count;
+            var alignedLength = data.Length - data.Length % vectorSize;
+            var left = new Vector<int>(L);
+            var right = new Vector<int>(R);
+            int count = 0;
+            for (int i = 0; i < alignedLength; i += vectorSize)
+            {
+                var slice = new Vector<int>(data, i);
+                var result = Vector.LessThanOrEqual(left, slice) & Vector.LessThanOrEqual(slice, right);
+                for (int s = 0; s < vectorSize; s++)
+                    count -= result[s];
+            }
+            return count;
+        }
+    }
+}

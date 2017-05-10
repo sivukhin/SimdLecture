1f013868e3089f3175118fed09a049f1c1fc8715
Umqra <golovoiapklavu@gmail.com>
2017-05-10 21:00:58 +0500

    Add DragonFractal project and rename other project
    SimdLecture -> SimdExamples

---
+++ SimdLecture/DragonFractal/DragonFractalTask.cs
@@ -0,0 +1,41 @@
+﻿// В этом пространстве имен содержатся средства для работы с изображениями. Чтобы оно стало доступно, в проект был подключен Reference на сборку System.Drawing.dll
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace Fractals
+{
+	internal static class DragonFractalTask
+	{
+		public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
+		{
+            var stopwatch = new Stopwatch();
+		    stopwatch.Start();
+
+		    var random = new Random(seed);
+		    double x = 1, y = 0;
+		    for (int i = 0; i < iterationsCount; i++)
+		    {
+		        var transformation = random.Next() & 1;
+		        double newX, newY;
+                if (transformation == 0)
+                { 
+		            newX = (x * Math.Cos(Math.PI / 4) - y * Math.Sin(Math.PI / 4)) / Math.Sqrt(2);
+                    newY = (x * Math.Sin(Math.PI / 4) + y * Math.Cos(Math.PI / 4)) / Math.Sqrt(2);
+                }
+                else
+                {
+                    newX = (x * Math.Sin(-Math.PI / 4) + y * Math.Cos(-Math.PI / 4)) / Math.Sqrt(2) + 1;
+                    newY = (x * Math.Cos(-Math.PI / 4) - y * Math.Sin(-Math.PI / 4)) / Math.Sqrt(2);
+                }
+		        x = newX;
+		        y = newY;
+                pixels.SetPixel(x, y);
+            }
+		    Console.WriteLine(stopwatch.ElapsedMilliseconds);
+		}
+	}
+}
